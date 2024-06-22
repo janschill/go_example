c014c1636902119595dd03fc2611b5cd754a02ce
Jan Schill <janschill@proton.me>
2024-06-22 22:44:01 +0200

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/janschill/go_example
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+var Buildstamp string
+var svnRevision string
+
+func FunctionForVetReport() {
+	var a int32 = 0
+
+	var wg sync.WaitGroup
+	for i := 0; i < 500; i++ {
+		wg.Add(1)
+		go func() {
+			a = atomic.AddInt32(&a, 1)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func main() {
+	fmt.Printf("Main: " + Buildstamp + " " + svnRevision + "\n")
+}
